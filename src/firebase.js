8342d8b8a0aecbaea95b9a373a3e18bd6f227bd0
Amrita <ayadav9095@gmail.com>
2025-05-03 08:54:42 +0530

    Initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCijHCsIfF5Rwsezp_YczwwR03XfKh2Fz8",
-  authDomain: "summer-camp-website-6cff0.firebaseapp.com",
-  projectId: "summer-camp-website-6cff0",
-  storageBucket: "summer-camp-website-6cff0.firebasestorage.app",
-  messagingSenderId: "657277796422",
-  appId: "1:657277796422:web:268363dba632cbd514ca47",
-  measurementId: "G-QGQMZH9ZR2"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
